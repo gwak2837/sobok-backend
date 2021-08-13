a567d465f767db499c0abe4f4d5077c5fce33eb3
Taeuk Gwak <gwak2837@kakao.com>
2021-08-13 23:43:56 +0900

    Add creating user and store SQL

--- database/sql/initialization.sql
+++ database/sql/initialization.sql
@@ -25,8 +25,8 @@ CREATE TABLE "user" (
   modification_time timestamptz NOT NULL DEFAULT NOW(),
   email varchar(50) NOT NULL UNIQUE,
   name varchar(50) NOT NULL,
-  gender char(4) NOT NULL DEFAULT '해당없음',
   phone varchar(20),
+  gender int NOT NULL DEFAULT 0,
   is_email_verified boolean NOT NULL DEFAULT FALSE,
   is_star_user boolean NOT NULL DEFAULT FALSE,
   --
@@ -46,17 +46,17 @@ CREATE TABLE store (
   creation_time timestamptz NOT NULL DEFAULT NOW(),
   modification_time timestamptz NOT NULL DEFAULT NOW(),
   name varchar(50) NOT NULL,
-  registration_number char(10) NOT NULL UNIQUE,
   town varchar(20) NOT NULL,
   address varchar(50) NOT NULL,
-  tel varchar(20) NOT NULL UNIQUE,
+  categories int [] NOT NULL,
   takeout boolean NOT NULL DEFAULT FALSE,
-  categories int[] NOT NULL,
   --
+  tel varchar(20) UNIQUE,
+  registration_number char(10) UNIQUE,
   description text,
   business_hours text,
   holiday text,
-  image_urls text[],
+  image_urls text [],
   user_id bigint REFERENCES "user" ON DELETE CASCADE -- 매장 소유자
 );
 
@@ -66,7 +66,7 @@ CREATE TABLE menu (
   modification_time timestamptz NOT NULL DEFAULT NOW(),
   name varchar(50) NOT NULL,
   price int NOT NULL,
-  image_urls text[] NOT NULL,
+  image_urls text [] NOT NULL,
   category int NOT NULL,
   store_id bigint NOT NULL REFERENCES store ON DELETE CASCADE -- 매장에 있는 메뉴
 );
@@ -76,30 +76,32 @@ CREATE TABLE news (
   creation_time timestamptz NOT NULL DEFAULT NOW(),
   modification_time timestamptz NOT NULL DEFAULT NOW(),
   title varchar(100) NOT NULL,
-  contents text[] NOT NULL,
+  contents text [] NOT NULL,
   category int NOT NULL,
-  store_id bigint NOT NULL REFERENCES store ON DELETE CASCADE, -- 소식을 작성한 매장
+  store_id bigint NOT NULL REFERENCES store ON DELETE CASCADE,
   --
-  image_urls text[]
+  image_urls text []
 );
 
+-- user_id: 피드를 작성한 사용자
+-- store_id: 피드에 태그된 매장
 CREATE TABLE feed (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   creation_time timestamptz NOT NULL DEFAULT NOW(),
   modification_time timestamptz NOT NULL DEFAULT NOW(),
   rating int NOT NULL,
-  contents text[] NOT NULL,
+  contents text [] NOT NULL,
+  image_urls text [] NOT NULL,
   like_count int NOT NULL DEFAULT 0,
-  image_urls text[] NOT NULL,
   user_id bigint NOT NULL REFERENCES "user" ON DELETE CASCADE,
   store_id bigint NOT NULL REFERENCES store ON DELETE CASCADE
 );
 
-CREATE TABLE comment (
+CREATE TABLE COMMENT (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   creation_time timestamptz NOT NULL DEFAULT NOW(),
   modification_time timestamptz NOT NULL DEFAULT NOW(),
-  contents text[] NOT NULL,
+  contents text [] NOT NULL,
   user_id bigint NOT NULL REFERENCES "user" ON DELETE CASCADE,
   feed_id bigint NOT NULL REFERENCES feed ON DELETE CASCADE,
   --
@@ -176,8 +178,8 @@ CREATE TABLE deleted."user" (
   deletion_time timestamptz NOT NULL DEFAULT NOW(),
   email varchar(50) NOT NULL UNIQUE,
   name varchar(50) NOT NULL,
-  gender char(4) NOT NULL DEFAULT '해당없음',
   phone varchar(20),
+  gender char(4) NOT NULL DEFAULT '해당없음',
   is_email_verified boolean NOT NULL DEFAULT FALSE,
   is_star_user boolean NOT NULL DEFAULT FALSE,
   --
@@ -204,7 +206,7 @@ CREATE TABLE deleted.store (
   description text,
   business_hours text,
   holiday text,
-  image_urls text[],
+  image_urls text [],
   user_id bigint REFERENCES deleted."user" ON DELETE CASCADE -- 매장 소유자
 );
 
@@ -214,7 +216,7 @@ CREATE TABLE deleted.menu (
   modification_time timestamptz NOT NULL DEFAULT NOW(),
   name varchar(50) NOT NULL,
   price int NOT NULL,
-  image_urls text[] NOT NULL,
+  image_urls text [] NOT NULL,
   store_id bigint NOT NULL REFERENCES deleted.store ON DELETE CASCADE -- 메뉴가 있는 매장
 );
 
@@ -223,23 +225,25 @@ CREATE TABLE deleted.news (
   creation_time timestamptz NOT NULL DEFAULT NOW(),
   modification_time timestamptz NOT NULL DEFAULT NOW(),
   title varchar(100) NOT NULL,
-  contents text[] NOT NULL,
+  contents text [] NOT NULL,
   category int NOT NULL,
-  store_id bigint NOT NULL REFERENCES deleted.store ON DELETE CASCADE, -- 소식을 작성한 매장
+  store_id bigint NOT NULL REFERENCES deleted.store ON DELETE CASCADE,
   --
-  image_urls text[]
+  image_urls text []
 );
 
+-- user_id: 피드를 작성한 사용자
+-- store_id: 피드에 태그된 매장
 CREATE TABLE deleted.feed (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   creation_time timestamptz NOT NULL DEFAULT NOW(),
   modification_time timestamptz NOT NULL DEFAULT NOW(),
   rating int NOT NULL,
-  contents text[] NOT NULL,
+  contents text [] NOT NULL,
+  image_urls text [] NOT NULL,
   like_count int NOT NULL DEFAULT 0,
-  image_urls text[] NOT NULL,
-  user_id bigint NOT NULL REFERENCES deleted."user" ON DELETE CASCADE, -- 피드를 작성한 사용자
-  store_id bigint NOT NULL REFERENCES deleted.store ON DELETE CASCADE, -- 피드에 태그된 매장
+  user_id bigint NOT NULL REFERENCES deleted."user" ON DELETE CASCADE,
+  store_id bigint NOT NULL REFERENCES deleted.store ON DELETE CASCADE,
   menu_id bigint NOT NULL REFERENCES deleted.menu ON DELETE CASCADE -- 피드에 태그된 메뉴
 );
 
@@ -247,7 +251,7 @@ CREATE TABLE deleted.comment (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   creation_time timestamptz NOT NULL DEFAULT NOW(),
   modification_time timestamptz NOT NULL DEFAULT NOW(),
-  contents text[] NOT NULL,
+  contents text [] NOT NULL,
   user_id bigint NOT NULL REFERENCES deleted."user" ON DELETE CASCADE,
   feed_id bigint NOT NULL REFERENCES deleted.feed ON DELETE CASCADE
 );
@@ -258,3 +262,139 @@ CREATE TABLE deleted.hashtag (
   name varchar(50) NOT NULL UNIQUE
 );
 
+CREATE FUNCTION create_user (
+  email varchar(50),
+  password_hash text,
+  name varchar(50),
+  phone varchar(20),
+  gender int,
+  bio varchar(50) DEFAULT NULL,
+  birth date DEFAULT NULL,
+  image_url text DEFAULT NULL,
+  out user_id bigint
+) LANGUAGE SQL AS $$
+INSERT INTO "user" (
+    email,
+    name,
+    phone,
+    gender,
+    bio,
+    birth,
+    image_url,
+    password_hash
+  )
+VALUES (
+    email,
+    name,
+    phone,
+    gender,
+    bio,
+    birth,
+    image_url,
+    password_hash
+  )
+RETURNING id;
+
+$$;
+
+CREATE FUNCTION create_store (
+  name varchar(50),
+  town varchar(20),
+  address varchar(50),
+  categories int [],
+  takeout boolean DEFAULT false,
+  tel varchar(20) DEFAULT NULL,
+  registration_number char(10) DEFAULT NULL,
+  description text DEFAULT NULL,
+  business_hours text DEFAULT NULL,
+  holiday text DEFAULT NULL,
+  image_urls text [] DEFAULT NULL,
+  user_id bigint DEFAULT NULL,
+  out user_id bigint
+) LANGUAGE SQL AS $$
+INSERT INTO store (
+    name,
+    town,
+    address,
+    categories,
+    takeout,
+    tel,
+    registration_number,
+    description,
+    business_hours,
+    holiday,
+    image_urls,
+    user_id
+  )
+VALUES (
+    name,
+    town,
+    address,
+    categories,
+    takeout,
+    tel,
+    registration_number,
+    description,
+    business_hours,
+    holiday,
+    image_urls,
+    user_id
+  )
+RETURNING id;
+
+$$;
+
+SELECT create_user(
+    'bok1@sindy.com',
+    '12345',
+    '김효진',
+    '010-6866-4135',
+    2,
+    '세상의 모든 디저트를 정복할꺼야!',
+    '1997-06-10',
+    'https://storage.googleapis.com/sobok/%EA%B9%80%ED%9A%A8%EC%A7%84.webp'
+  );
+
+SELECT create_user(
+    'bok2@sindy.com',
+    '12345',
+    '곽태욱',
+    '010-9203-2837',
+    1,
+    '크로플 같은 남자',
+    '1998-04-12',
+    'https://storage.googleapis.com/sobok/%EA%B3%BD%ED%83%9C%EC%9A%B1.webp'
+  );
+
+SELECT create_user(
+    'bok3@sindy.com',
+    '12345',
+    '박수현',
+    '010-4536-7393',
+    2,
+    '대구의 모든 맛집!',
+    '1997-05-05',
+    'https://storage.googleapis.com/sobok/%EB%B0%95%EC%88%98%ED%98%84.webp'
+  );
+
+SELECT create_user(
+    'bok4@sindy.com',
+    '12345',
+    '김민호',
+    '010-7644-5980',
+    1,
+    '디저트에 빠진 자취남(자아도취남)',
+    '1996-09-12',
+    'https://storage.googleapis.com/sobok/%EA%B9%80%EB%AF%BC%ED%98%B8.webp'
+  );
+
+SELECT create_user(
+    'bok5@sindy.com',
+    '12345',
+    '기현우',
+    '010-2563-6996',
+    1,
+    '아재입맛, 집 근처만 다닙니다',
+    '1995-01-14',
+    'https://storage.googleapis.com/sobok/%EA%B8%B0%EC%9A%B0%ED%98%84.webp'
+  );
