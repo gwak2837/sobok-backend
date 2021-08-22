5feb9c5b5c54f3718cf4036f73a4cf2f8a6baafd
Taeuk Gwak <gwak2837@kakao.com>
2021-08-22 20:46:29 +0900

    Add type generator from PostgreSQL

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-/* eslint-disable import/first */
-import dotenv from 'dotenv'
-
-if (process.env.NODE_ENV !== 'production') dotenv.config({ path: '.env.development' })
-dotenv.config()
-
 import { connectDatabase } from './database/postgres'
 import { startApolloExpressServer } from './common/express'
 import { connectRedis } from './common/redis'
