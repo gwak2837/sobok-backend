87f88fec874f6e216655bb71513f0893316e9b05
Taeuk Gwak <gwak2837@kakao.com>
2021-08-25 09:05:35 +0900

    Fix error of fetching stores

--- src/graphql/store/Query.resolver.ts
+++ src/graphql/store/Query.resolver.ts
@@ -5,6 +5,7 @@ import { poolQuery } from '../../database/postgres'
 import { selectColumnFromField } from '../../utils/ORM'
 import { storeFieldColumnMapping, storeORM } from './ORM'
 
+const stores = importSQL(__dirname, 'sql/stores.sql')
 const storesByCategories = importSQL(__dirname, 'sql/storesByCategories.sql')
 const storesByTown = importSQL(__dirname, 'sql/storesByTown.sql')
 const storesByTownAndCategories = importSQL(__dirname, 'sql/storesByTownAndCategories.sql')
@@ -13,22 +14,28 @@ export const Query: QueryResolvers = {
   storesByTownAndCategories: async (_, { categories, town }, ___, info) => {
     const columns = selectColumnFromField(info, storeFieldColumnMapping)
 
-    if (!categories) {
+    if (town && categories) {
+      const { rows } = await poolQuery(format(await storesByTownAndCategories, columns), [
+        town,
+        categories,
+      ])
+
+      return rows.map((row) => storeORM(row))
+    }
+
+    if (town) {
       const { rows } = await poolQuery(format(await storesByTown, columns), [town])
 
       return rows.map((row) => storeORM(row))
     }
 
-    if (!town) {
+    if (categories) {
       const { rows } = await poolQuery(format(await storesByCategories, columns), [categories])
 
       return rows.map((row) => storeORM(row))
     }
 
-    const { rows } = await poolQuery(format(await storesByTownAndCategories, columns), [
-      town,
-      categories,
-    ])
+    const { rows } = await poolQuery(format(await stores, columns))
 
     return rows.map((row) => storeORM(row))
   },
