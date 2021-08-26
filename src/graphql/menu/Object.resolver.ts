3ef3be847ba2574b1e023ecb14f194b526bb43f0
Taeuk Gwak <gwak2837@kakao.com>
2021-08-26 17:37:05 +0900

    Add sql files of store, hashtags field of menu

---
+++ src/graphql/menu/Object.resolver.ts
@@ -0,0 +1,27 @@
+import format from 'pg-format'
+import type { MenuResolvers } from 'src/graphql/generated/graphql'
+import { importSQL } from '../../utils/commons'
+import { poolQuery } from '../../database/postgres'
+import { selectColumnFromField } from '../../utils/ORM'
+import { storeFieldColumnMapping, storeORM } from '../store/ORM'
+
+const hashtags = importSQL(__dirname, 'sql/hashtags.sql')
+const store = importSQL(__dirname, 'sql/store.sql')
+
+export const Menu: MenuResolvers = {
+  hashtags: async ({ id }, __) => {
+    const { rows } = await poolQuery(await hashtags, [id])
+
+    console.log(rows)
+
+    return rows.map((row) => row.name)
+  },
+
+  store: async ({ storeId }, __, ___, info) => {
+    const columns = selectColumnFromField(info, storeFieldColumnMapping)
+
+    const { rows } = await poolQuery(format(await store, columns), [storeId])
+
+    return storeORM(rows[0])
+  },
+}
