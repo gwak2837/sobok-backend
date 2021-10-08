dfbdda372a4b11e251b98d5d014a13c09d949ce0
Taeuk Gwak <gwak2837@kakao.com>
2021-10-08 12:08:46 +0900

    Minor changes

--- database/sql/test.sql
+++ database/sql/test.sql
@@ -1,1 +1,17 @@
-call create_bucket('a', 1);
+SELECT id,
+  name
+FROM menu
+WHERE (name, id) < ('아메리카노', 1)
+ORDER BY name DESC,
+  id DESC
+FETCH FIRST 5 ROWS ONLY;
+
+SELECT id,
+  name,
+  price
+FROM menu
+WHERE id > 1
+ORDER BY price
+FETCH FIRST 5
+  /* =limit */
+  ROWS ONLY
