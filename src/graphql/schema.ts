de95f7ea7a30adadc390df27b32356f0147def27
Taeuk Gwak <gwak2837@kakao.com>
2021-10-08 13:53:37 +0900

    Modify method of importing graphql files

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,15 +1,14 @@
 import { mergeResolvers } from '@graphql-tools/merge'
 import { makeExecutableSchema } from '@graphql-tools/schema'
 
-import * as bucketResolver from './bucket/Query.resolver'
+import * as bucketObjectResolver from './bucket/Object.resolver'
+import * as bucketQueryResolver from './bucket/Query.resolver'
+import * as scalarResolver from './common/scalar.resolver'
 import typeDefs from './generated/schema.graphql'
 
-// loadFilesSync 실행 시 *.resolver.* 파일에서 모듈을 절대 경로로 불러오면 오류 발생, 꼭 상대 경로로 모듈을 불러와야 함
-const resolversArray = [bucketResolver] // loadFilesSync(join(__dirname, '**/*.resolver.*'))
-console.log(resolversArray)
+const resolversArray = [bucketQueryResolver, bucketObjectResolver, scalarResolver]
 
 const resolvers = mergeResolvers(resolversArray)
-console.log(resolvers)
 
 const schema = makeExecutableSchema({ typeDefs, resolvers })
 
