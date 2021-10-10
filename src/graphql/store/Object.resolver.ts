f5cde93ae2fb7f09e98dee4deb3b9203525d6d1b
Taeuk Gwak <gwak2837@kakao.com>
2021-10-11 01:08:26 +0900

    Block deep field resolver

--- src/graphql/store/Object.resolver.ts
+++ src/graphql/store/Object.resolver.ts
@@ -1,3 +1,4 @@
+import { NotImplementedError } from '../../apollo/errors'
 import type { StoreResolvers } from '../generated/graphql'
 import { decodeCategories } from './ORM'
 
@@ -12,5 +13,15 @@ export const Store: StoreResolvers = {
 
   isLiked: ({ isLiked }) => {
     return !!isLiked
+  },
+
+  menus: ({ menus }) => {
+    if (!menus) throw new NotImplementedError('menus 필드 리졸버가 구현되지 않았습니다.')
+    return menus
+  },
+
+  hashtags: ({ hashtags }) => {
+    if (!hashtags) throw new NotImplementedError('hashtags 필드 리졸버가 구현되지 않았습니다.')
+    return hashtags
   },
 }
