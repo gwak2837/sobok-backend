3962a9d213ff1149de5daf26f9b10da3f84828f3
Taeuk Gwak <gwak2837@kakao.com>
2021-10-13 22:08:15 +0900

    Rename files

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,20 +1,21 @@
 import { mergeResolvers } from '@graphql-tools/merge'
-import { makeExecutableSchema } from '@graphql-tools/schema'
 
 // import * as bucketObjectResolver from './bucket/Object.resolver'
 // import * as bucketQueryResolver from './bucket/Query.resolver'
 // import * as commentObjectResolver from './comment/Object.resolver'
-import * as commentQueryResolver from './comment/Query.resolver'
+import * as commentQueryResolver from './comment/Query'
 import * as commonResolver from './common/common.resolver'
-// import * as feedObjectResolver from './feed/Object.resolver'
-// import * as feedQueryResolver from './feed/Query.resolver'
-import typeDefs from './generated/schema.graphql'
 // import * as menuObjectResolver from './menu/Object.resolver'
 // import * as menuQueryResolver from './menu/Query.resolver'
 // import * as newsObjectResolver from './news/Object.resolver'
 // import * as newsQueryResolver from './news/Query.resolver'
-import * as storeObjectResolver from './store/Object.resolver'
-import * as storeQueryResolver from './store/Query.resolver'
+import * as storeObjectResolver from './store/Object'
+import * as storeQueryResolver from './store/Query'
+
+// import { makeExecutableSchema } from '@graphql-tools/schema'
+
+// import * as feedObjectResolver from './feed/Object.resolver'
+// import * as feedQueryResolver from './feed/Query.resolver'
 
 // import * as userMutationResolver from './user/Mutation.resolver'
 // import * as userObjectResolver from './user/Object.resolver'
@@ -38,8 +39,4 @@ const resolversArray = [
   // userQueryResolver,
 ]
 
-const resolvers = mergeResolvers(resolversArray)
-
-const schema = makeExecutableSchema({ typeDefs, resolvers })
-
-export default schema
+export const resolvers = mergeResolvers(resolversArray)
