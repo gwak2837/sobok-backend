3356dbe83b4f152119a9cb08a8fc5ab1833e320e
Taeuk Gwak <gwak2837@kakao.com>
2021-10-19 20:33:54 +0900

    Sample jest testing codes

--- test/sql/test.sql
+++ test/sql/test.sql
@@ -1,6 +1,18 @@
-SELECT store.id,
-  store.name
-FROM store
-  JOIN store_x_hashtag ON store_x_hashtag.store_id = store.id
-  JOIN hashtag ON hashtag.id = store_x_hashtag.hashtag_id
-  AND hashtag.name = ANY(ARRAY ['케이크', '통유리'])
+SELECT menu.id,
+  menu.name,
+  menu.price,
+  menu.image_urls,
+  store.id,
+  store.name,
+  store.point,
+  user_x_liked_menu.user_id AS is_liked,
+  array_agg(hashtag.name) AS hashtags
+FROM menu
+  JOIN store ON store.id = menu.store_id
+  LEFT JOIN menu_x_hashtag ON menu_x_hashtag.menu_id = menu.id
+  JOIN hashtag ON hashtag.id = menu_x_hashtag.hashtag_id
+  LEFT JOIN user_x_liked_menu ON user_x_liked_menu.menu_id = menu.id
+  AND user_x_liked_menu.user_id = '082a8bb3-2eb1-44a3-9efe-bc906827c49f'
+GROUP BY menu.id,
+  store.id,
+  user_x_liked_menu.user_id
