1909c5d0197797ca937c64ee97f27da3b6bfd316
Taeuk Gwak <gwak2837@kakao.com>
2021-10-24 21:56:15 +0900

    Rename function

--- src/graphql/news/Query.ts
+++ src/graphql/news/Query.ts
@@ -3,7 +3,11 @@ import { AuthenticationError } from 'apollo-server-express'
 import { NotFoundError } from '../../apollo/errors'
 import type { ApolloContext } from '../../apollo/server'
 import { poolQuery } from '../../database/postgres'
-import { buildSQL } from '../../utils/sql'
+import {
+  applyPaginationAndSorting,
+  buildSelect,
+  validatePaginationAndSorting,
+} from '../../utils/sql'
 import { graphqlRelationMapping } from '../common/ORM'
 import { NewsOptions, QueryResolvers } from '../generated/graphql'
 import { validateStoreCategories } from '../store/ORM'
@@ -25,16 +29,19 @@ export const Query: QueryResolvers<ApolloContext> = {
     return graphqlRelationMapping(rows[0], 'news')
   },
 
-  newsListByStore: async (_, { storeId, categories }) => {
+  newsListByStore: async (_, { storeId, categories, order, pagination }) => {
     const encodedStoreCategories = validateStoreCategories(categories)
+    validatePaginationAndSorting(order, pagination)
 
     let sql = newsListByStore
     const values: unknown[] = [storeId]
 
     if (categories) {
-      sql = buildSQL(sql, 'WHERE', whereNewsCategory)
+      sql = buildSelect(sql, 'WHERE', whereNewsCategory)
       values.push(encodedStoreCategories)
     }
+
+    sql = applyPaginationAndSorting(sql, values, 'news', order, pagination)
 
     const { rowCount, rows } = await poolQuery(sql, values)
     if (rowCount === 0) throw new NotFoundError('해당 조건의 피드를 찾을 수 없습니다.')
@@ -42,14 +49,15 @@ export const Query: QueryResolvers<ApolloContext> = {
     return rows.map((row) => graphqlRelationMapping(row, 'news'))
   },
 
-  newsListByTown: async (_, { town, option, categories }, { userId }) => {
+  newsListByTown: async (_, { town, option, categories, order, pagination }, { userId }) => {
     const encodedStoreCategories = validateStoreCategories(categories)
+    validatePaginationAndSorting(order, pagination)
 
     let sql = newsListByTown
     const values: unknown[] = []
 
     if (town) {
-      sql = buildSQL(sql, 'WHERE', whereStoreTown)
+      sql = buildSelect(sql, 'WHERE', whereStoreTown)
       values.push(town)
     }
 
@@ -57,14 +65,16 @@ export const Query: QueryResolvers<ApolloContext> = {
       if (!userId)
         throw new AuthenticationError('찜한 매장의 소식을 보고 싶다면 로그인 후 시도해주세요.')
 
-      sql = buildSQL(sql, 'JOIN', joinLikedStore)
+      sql = buildSelect(sql, 'JOIN', joinLikedStore)
       values.push(userId)
     }
 
     if (categories) {
-      sql = buildSQL(sql, 'WHERE', whereStoreCategories)
+      sql = buildSelect(sql, 'WHERE', whereStoreCategories)
       values.push(encodedStoreCategories)
     }
+
+    sql = applyPaginationAndSorting(sql, values, 'news', order, pagination)
 
     const { rowCount, rows } = await poolQuery(sql, values)
     if (rowCount === 0) throw new NotFoundError('해당 조건의 피드를 찾을 수 없습니다.')
